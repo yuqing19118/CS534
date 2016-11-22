61d23c4ea6164535d958a70b5ff671a9d94eb2a4
Mary Feng <maryfeng@cs.umass.edu>
2016-11-21 21:33:33 -0600

    Outlined main; to do BestMatch

---
+++ BestMatch.m
@@ -0,0 +1,6 @@
+function [ x, y ] = BestMatch( gpA, gpAprime, gpB, gpBprime, s, level, q )
+% BestMatch using FLANN and BestCoherenceMatch
+
+
+end
+
--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 clear;
 
 % read input images
-A =  imread('images/A.jpg');
-Aprime =  imread('images/Aprime.jpg');
-B =  imread('images/B.jpg');
+A = imread('images/A.jpg');
+Aprime = imread('images/Aprime.jpg');
+B = imread('images/B.jpg');
 
 % RGB to YIQ color space
 yiqA = rgb2ntsc(A);
@@ -27,3 +27,26 @@ gpB = ComputeGaussianPyramid(yB);
 featuresA = ComputeFeatures(gpA);
 featuresAprime = ComputeFeatures(gpAprime);
 featuresB = ComputeFeatures(gpB);
+
+% compute Bprime, pixel by pixel
+gpBprime = cell(1,size(featuresB,2));
+s = cell(1,size(featuresB,2));
+for level = size(gpBprime,2):1
+    for row = 1:size(yB,1)
+        for col = 1:size(yB,2)
+            q = gpBprime{level}(row, col);
+            [ x, y ] = BestMatch(gpA, gpAprime, gpB, gpBprime, s, level, q);
+            gpBprime{level}(row, col) = gpAprime{level}(x,y);
+            s{level}(row, col) = [ x, y ];
+        end
+    end
+end
+
+% get Y channel of Bprime
+yBprime = gpBprime{1};
+% combine with original IQ channels
+yiqBprime = cat(3, yBprime,yiqB(:,:,2),yiqB(:,:,3));
+
+% YIQ to RGB
+Bprime = ntsc2rgb(yiqBprime);
+imwrite(Bprime, 'images/Bprime.jpg', 'jpg');
