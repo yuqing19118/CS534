c2769f83cd050a24c436fce6254f2c26dc1f0b42
Mary Feng <maryfeng@cs.umass.edu>
2016-12-12 01:23:31 -0600

    Fix error in LuminanceRemapping

--- LuminanceRemapping.m
+++ LuminanceRemapping.m
@@ -4,6 +4,6 @@ function [ remapA, remapAprime ] = LuminanceRemapping( yA, yAprime, yB )
 % remap luminance channel of A
 remapA = (std2(yB)/std2(yA)) * (yA-mean2(yA)) + mean2(yB);
 % remap luminance channel of Aprime for consistency
-remapAprime = (std2(yB)/std2(yAprime)) * (yA-mean2(yAprime)) + mean2(yB);
+remapAprime = (std2(yB)/std2(yAprime)) * (yAprime-mean2(yAprime)) + mean2(yB);
 
 end
